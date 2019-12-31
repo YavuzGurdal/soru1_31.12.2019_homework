4b3b8f97584c02dc406bba778378d6673905329c
YavuzGurdal <57366318+YavuzGurdal@users.noreply.github.com>
2019-12-31 19:50:26 +0100

    Add files via upload

---
+++ soru1.js
@@ -0,0 +1,37 @@
+/*
+1. Problem :
+    Parametre olarak bir array alacak. 
+    Array içerisinde string veya number tipi elemanlar olabilir. 
+    Bu array içerisinde tekrarlanan elemanlardan olusan yeni bir array'i console'a 
+    yazdıran fonksiyonu yazmak. 
+
+    Örneğin : 
+    console.log(func_name ([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));  ---> [4, 7] 
+    olacak şekilde.
+ */
+
+
+ 
+let tekrarEdenElemanlar = [];
+
+let tekrarEdenDegerleriBul = function (array) {
+
+let i,j;
+
+for ( i = 0; i < array.length; i++) {
+
+    for ( j = i + 1; j < array.length; j++) {
+
+        if (array[i] == array[j] && tekrarEdenElemanlar.indexOf(array[i]) == -1 ) {
+           // indexOf = - 1 ise bu deger indexte yok demektir
+           
+           tekrarEdenElemanlar.push(array[i]);
+        }
+    }
+}
+
+return tekrarEdenElemanlar;
+
+}
+
+console.log(tekrarEdenDegerleriBul ([2, 3, 5, 6, 6, 2, -2, -2, 'ali', 'veli', 'ali']));
